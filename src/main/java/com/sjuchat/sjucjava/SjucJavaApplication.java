b52df610656f30bb890b3af35de736e81749a087
lipeng <106864183+damokeris@users.noreply.github.com>
2023-09-13 18:26:45 +0800

    初始化

---
+++ src/main/java/com/sjuchat/sjucjava/SjucJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sjuchat.sjucjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjucJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjucJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sjuchat/sjucjava/SjucJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjuchat.sjucjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjucJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
